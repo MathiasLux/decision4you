f824ff0cbef18f0cb17e21ec43116cc1e03c2983
MathiasLux <lux19959@spengergasse.at>
2023-12-19 17:53:47 +0100

    Initial Commit

---
+++ src/main/java/com/project/d4u/D4uApplication.java
@@ -0,0 +1,13 @@
+package com.project.d4u;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D4uApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D4uApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/d4u/D4uApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.d4u;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D4uApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
