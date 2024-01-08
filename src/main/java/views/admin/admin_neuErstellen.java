99f9f447206b6ad084b45117a7d8ecfacb831c50
MathiasLux <lux19959@spengergasse.at>
2024-01-08 17:24:56 +0100

    Funktionierende SpringBoot

--- src/main/java/views/admin/admin_neuErstellen.java
+++ src/main/java/views/admin/admin_neuErstellen.java
@@ -1,5 +1,16 @@
 package views.admin;
 
-public class admin_neuErstellen
-{
+
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+
+
+@Route("") // map view to the root
+class admin_neuErstellen extends VerticalLayout {
+    admin_neuErstellen() {
+        add(new H1("Hello, world"));
+    }
 }
